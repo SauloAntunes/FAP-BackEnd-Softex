b40f047d4d56a31193a2f10f84cd077ed5e05fa3
SauloAntunes <134818469+SauloAntunes@users.noreply.github.com>
2023-10-13 21:18:59 -0300

    Create server.js

---
+++ Padrões de Desenvolvimento de Software/Conteúdo das aulas presenciais/Aula_13_10/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const routes = require('./routes');
+const bodyParser = require('body-parser');
+const dataBase = require('./db');
+const User = require('./models/userModel');
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(3000, () => {
+    console.log('Servidor inicializado!');
+});
+
+
+dataBase.sync();
